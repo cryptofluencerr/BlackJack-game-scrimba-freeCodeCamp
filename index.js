b3fdfed962f1bd1b5fb0445bfe7b3f8b62785357
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-09-13 16:36:02 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
-let firstCard = Math.floor(Math.random() * 10 + 1);
-let secondCard = Math.floor(Math.random() * 10 + 1);
+let firstCard = 0;
+let secondCard = 0;
+let sum = 0;
+let cards = [];
+let startEl = document.getElementById("start-el");
+let sumEl = document.querySelector("#sum-el");
+let messageEl = document.querySelector("#message-el");
+//getElementById and querySelector here works the same
+let cardsEl = document.getElementById("cards-el");
+let hasBlackJack = false;
+let isAlive = true;
 
-let sum = firstCard + secondCard + 6;
-console.log(sum);
+let player = { name: "Kc", chips: 145 };
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
-if (sum < 21) {
-  console.log("Do you want to draw a new card?");
-} else if (sum === 21) {
-  console.log("Wohoo! You've got a blackjack! ");
-} else {
-  console.log("You're out of the game!");
+//Get random number
+function getRandom() {
+  let randomNum = Math.floor(Math.random() * 13 + 1);
+  if (randomNum === 1) {
+    randomNum = 11;
+  } else if (randomNum > 10) {
+    randomNum = 10;
+  } else {
+    randomNum = randomNum;
+  }
+  return randomNum;
+}
+
+//Starting game
+function start() {
+  firstCard = getRandom();
+  secondCard = getRandom();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+//Start game code
+function renderGame() {
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got a blackjack! ";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  counting = () => {
+    for (let i = 0; i < cards.length + 1; i++) {
+      return (cardsEl.textContent = "Cards: " + cards);
+    }
+  };
+
+  // Render out first and second card
+  counting();
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack == false) {
+    let card = getRandom();
+    sum = card + sum;
+    renderGame();
+    cards.push(card);
+  }
+
+  // Render out all the cards being used
+  counting();
 }
